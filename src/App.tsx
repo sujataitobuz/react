3439f2c60dd43c4ff9f452bc9e360450b28bccd8
Sujata Biswas <sujata@itobuz.com>
2023-06-02 17:40:17 +0530

    first push

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './screens/Layout/Layout';
+import Home from './screens/Home/Home';
+import Blogs from './screens/Blogs/Blogs';
+import Contact from './screens/Contact/Contact';
+import PageNotFound from './screens/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/screens/Layout/Layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <div>
+      <div className='border-b-2 border-slate-300 flex justify-between items-center px-5 py-2'>
+        <p className="font-normal text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+        <div>
+        <button className="border border-red-700 px-3 py-1 rounded-md capitalize text-red-700" onClick={() => console.log('clicked')}>button</button>
+        <button className="border border-red-700 px-3 py-1 ml-2 rounded-md  text-red-700" onClick={() => console.log('clicked')}>button</button>
+        </div>
+      </div>
+      <nav className="bg-white px-5 py-4 flex justify-between items-center mb-8 drop-shadow-md">
+        <img src='https://media.licdn.com/dms/image/C4D0BAQGbMF7PV0i8-A/company-logo_200_200/0/1620126099335?e=2147483647&v=beta&t=tnCsp9_hFT1FyKEHmKB_4e0dE_3kOkFMVqCOgHazzZo' alt="logo" className='w-[50px]' />
+        <ul>
+          <li className='text-black font-normal text-sm md:text-xmd'>
+            <Link to="/"><span className='ml-5'>Home</span></Link>
+            <Link to="/blogs"><span className='ml-5'>Blogs</span></Link>
+            <Link to="/contact"><span className='ml-5'>Contact</span></Link>
+            <Link to="*"><span className='ml-5'>Another</span></Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </div>
+  )
+}
